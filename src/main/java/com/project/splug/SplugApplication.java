e9b9cb317f81f4086eaf14b36795a6b7bc2557bd
김도영 <rlaehdud4992@ssu.ac.kr>
2020-09-28 16:11:36 +0900

    프로젝트 초기 구성

---
+++ src/main/java/com/project/splug/SplugApplication.java
@@ -0,0 +1,13 @@
+package com.project.splug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/splug/SplugApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.project.splug;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SplugApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}

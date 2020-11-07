d2c7714aff4fdbb43ee841baa0f33d421463768e
김도영 <rlaehdud4992@ssu.ac.kr>
2020-11-07 15:36:14 +0900

    [hotfix] 자동 배포 스크립트 최종 수정
    장애 히스토리
    1. ^M이 스크립트 파일에 기입되어 인터프리터 실행 불가 문제
     - 파일 포멧을 unix로 변경하여 해결
    
    2. 빌드 파일 경로 오표기
     - 올바른 경로로 수정
    
    3. 웹 애플리케이션 프로세스 이름 오표기
     - 올바른 프로세스 명으로 변경

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # 스크립트 내 변수 설정
-REPOSITORY=/home/ubuntu/app/
+REPOSITORY=/home/ubuntu/app
 PROJECT_NAME=splug-springboot-webservice
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 PID 확인"
-# splug 이름과 정확히 매칭되는 jar 프로세스 탐색하여 이를 CURRENT_PID에 저장
-CURRENT_PID=$(pgrep -fl splug | grep jar | awk '{print $1}')
+# 현재 실행 중인 java 웹 애플리케이션의 PID 저장
+CURRENT_PID=$(pgrep -fl java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -26,7 +26,7 @@ else
   sleep 5
 fi
 
-# 가장 최신에 배포된 splug.jar 파일을 JAR_NAME에 저장
+# 가장 최신에 배포된 jar 파일을 JAR_NAME에 저장
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
@@ -35,7 +35,7 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-# JAR_NAME을 백그라운드로 실행 & nohup.out 파일을 만들지 않고 표준 입출력으로 사용
+# JAR_NAME을 백그라운드로 실행 & nohup.out 파일을 표준 입출력으로 사용
 nohup java -jar \
 -Dspring.config.location=classpath:/application.properties \
 -Dspring.profiles.active=real \

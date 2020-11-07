4cfe77e2f1a4ae0e592573e27e7899ea5e7c53d9
Kimdoyoung <52124111+dev-kimdoyoung@users.noreply.github.com>
2020-11-07 13:54:51 +0900

    [feature] 배포 자동화 코드 작성 (#21)
    1. 작업 내용
    - 배포 자동화 스크립트 코드 작성: deploy.sh
    - EC2에 배포 후 아래 작업을 진행하도록 설정: appspec.yml
       - CodeDeploy로부터 받은 파일들이 관리자 권한을 가지도록 설정
       - 새로운 파일들을 전달받고 deploy.sh 프로그램을 관리자 권한으로 실행
    
    resolves: #7
    see also:
    
    Co-authored-by: 김도영 <rlaehdud4992@ssu.ac.kr>

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+# 스크립트 내 변수 설정
+REPOSITORY=/home/ubuntu/app/
+PROJECT_NAME=splug-springboot-webservice
+
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동 중인 애플리케이션 PID 확인"
+# splug 이름과 정확히 매칭되는 jar 프로세스 탐색하여 이를 CURRENT_PID에 저장
+CURRENT_PID=$(pgrep -fl splug | grep jar | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+# CURRENT_PID에 값이 없으면 (길이가 0이면)
+if [ -z "$CURRENT_PID" ]; then
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  # 현재 실행 중인 프로세스를 정상적인 방법 (15)으로 종료
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+
+  # 5초 동안 대기 (wait)
+  sleep 5
+fi
+
+# 가장 최신에 배포된 splug.jar 파일을 JAR_NAME에 저장
+echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+echo "> $JAR_NAME 에 실행권한 추가"
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+# JAR_NAME을 백그라운드로 실행 & nohup.out 파일을 만들지 않고 표준 입출력으로 사용
+nohup java -jar \
+-Dspring.config.location=classpath:/application.properties \
+-Dspring.profiles.active=real \
+$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
